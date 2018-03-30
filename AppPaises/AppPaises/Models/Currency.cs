b1b1e9f2ef17a497a05930f4e6df1f082ac61471
Thirty33 <joelsuarez.1101@gmail.com>
2018-03-30 16:57:18 -0500

    Creando Models segun Api request

---
+++ AppPaises/AppPaises/Models/Currency.cs
@@ -0,0 +1,15 @@
+﻿namespace AppPaises.Models
+{
+    using Newtonsoft.Json;
+    public class Currency
+    {
+        [JsonProperty(PropertyName = "code")]
+        public string Code { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "symbol")]
+        public string Symbol { get; set; }
+    }
+}
