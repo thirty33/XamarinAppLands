dd78e2b588586ced61350bf12a246e677cfda05b
Thirty33 <joelsuarez.1101@gmail.com>
2018-03-30 12:58:10 -0500

    InstaceLocator, diccionario de recursos, binding Login en LoginPage.xaml

--- AppPaises/AppPaises/App.xaml.cs
+++ AppPaises/AppPaises/App.xaml.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
-
-namespace AppPaises
+﻿namespace AppPaises
 {
-	public partial class App : Application
+    using AppPaises.Views;
+    using Xamarin.Forms;
+
+    public partial class App : Application
 	{
 		public App ()
 		{
 			InitializeComponent();
 
-			MainPage = new AppPaises.MainPage();
+            //MainPage = new AppPaises.MainPage();
+            this.MainPage = new NavigationPage(new LoginPage());
 		}
 
 		protected override void OnStart ()
---
+++ AppPaises/AppPaises/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppPaises.ViewModels
+{
+    public class MainViewModel
+    {
+
+    }
+}
