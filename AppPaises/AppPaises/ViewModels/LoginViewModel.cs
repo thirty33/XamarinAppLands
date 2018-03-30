172d94796399304d09df54108f3b43070295c0ec
Thirty33 <joelsuarez.1101@gmail.com>
2018-03-30 13:20:54 -0500

    Implementando LoginViewModel, BaseViewModel, Enlazando contento en LoginPage

---
+++ AppPaises/AppPaises/ViewModels/LoginViewModel.cs
@@ -0,0 +1,67 @@
+﻿namespace AppPaises.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using System.Windows.Input;
+    using Views;
+    using Xamarin.Forms;
+
+    public class LoginViewModel : BaseViewModel
+    {
+        #region Attributes
+        private string email;
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
+        #region Properties
+        public string Email
+        {
+            get { return this.email; }
+            set { SetValue(ref this.email, value); }
+        }
+
+        public string Password
+        {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
+        }
+
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        private async void Login()
+        {
+
+        }
+
+        #endregion
+
+
+    }
+}
--- AppPaises/AppPaises/ViewModels/MainViewModel.cs
+++ AppPaises/AppPaises/ViewModels/MainViewModel.cs
@@ -1,11 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AppPaises.ViewModels
+﻿namespace AppPaises.ViewModels
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
     public class MainViewModel
     {
+        #region ViewModels
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
 
+
+        #region Constructors
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
     }
 }
