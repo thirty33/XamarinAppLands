4a5b1f00f6c0b753542a3cd35995d9a3381f5ac1
Thirty33 <joelsuarez.1101@gmail.com>
2018-04-03 08:49:51 -0500

    Estructura principal PaisPage, implementacion de Comando SelectLandCommand

--- AppPaises/AppPaises/ViewModels/PaisItemViewModel.cs
+++ AppPaises/AppPaises/ViewModels/PaisItemViewModel.cs
@@ -1,8 +1,31 @@
 ﻿namespace AppPaises.ViewModels
 {
-    using AppPaises.Models;
+    using GalaSoft.MvvmLight.Command;
+    using Models;
+    using System.Windows.Input;
+    using Views;
+    using Xamarin.Forms;
+
     public class PaisItemViewModel : Land
     {
+        #region Comandos
+        public ICommand SelectLandCommand
+        {
+            get
+            {
+                return new RelayCommand(SelectLand);
+            }
+        }
+        #endregion
 
+
+        #region Metodos
+        private async void SelectLand()
+        {
+            //Instancio la propiedad del locator y paso el item (this)
+            MainViewModel.GetInstance().Land = new PaisViewModel(this);
+            await Application.Current.MainPage.Navigation.PushAsync(new PaisPage());
+        }
+        #endregion
     }
 }
--- AppPaises/AppPaises/ViewModels/PaisViewModel.cs
+++ AppPaises/AppPaises/ViewModels/PaisViewModel.cs
@@ -1,6 +1,21 @@
 ﻿namespace AppPaises.ViewModels
 {
+    using Models;
     public class PaisViewModel
     {
+        #region Propiedades
+        public Land Land
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructor
+        public PaisViewModel(Land land)
+        {
+            this.Land = land;
+        }
+        #endregion
     }
 }
